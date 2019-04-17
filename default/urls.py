a4b7422de0ad0d31554ec8f3356399f204f9d35b
Kaiyu Tsao <i13585904041@gmail.com>
2019-04-17 15:40:14 +0800

    投票完成

--- default/urls.py
+++ default/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('poll/', poll_list),
-    path('poll1/', PollList.as_view()),
+    #path('poll/', poll_list),
+    path('poll/', PollList.as_view()),
+    path('poll/<int:pk>/', PollDetail.as_view()),
+    path('vote/<int:oid>/',PollVote.as_view()),
 ]
